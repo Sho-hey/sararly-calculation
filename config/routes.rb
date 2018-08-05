2b60f163f4f1f9060826a6ac386b144b6ed78f72
宮田昌平 <heyheyshohey0104@gmail.com>
2018-08-05 12:28:02 +0900

    write users#index
    user#indexでサイドバーのみをだし、staffs#indexで計算画面を出せるようにした。本来はcalculations#indexで計算画面を出したいが後から記述を変更するつもりである。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'staffs#index'
+  root 'users#index'
   resources :staffs, only: [:index]
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
 end
