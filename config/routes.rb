f9059b810fe01409463c9f7d31856bb27b4cac25
宮田昌平 <heyheyshohey0104@gmail.com>
2018-08-05 14:00:32 +0900

    create calculation routes controller view
    dbを介さないcalculationの ルーティング、コントローラー、ビューを作成しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
-  resources :staffs, only: [:index]
   resources :users, only: [:index, :edit, :update]
+  resources :staffs, only: [:index]
+  resources :calculations, only: [:index]
   resources :chanpagnes, only: [:index]
 end
