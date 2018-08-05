fcf6229ef714fb57be053ae9d6eade95c63ec37e
宮田昌平 <heyheyshohey0104@gmail.com>
2018-08-05 15:08:01 +0900

    create view staff
    staff一覧のビューを作成しました。
    編集、追加、削除ボタンをつけなくてはならない

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -1,4 +1,13 @@
 class StaffsController < ApplicationController
   def index
+    @staff = Staff.new
+  end
+
+  def create
+  end
+
+  private
+  def set_staff
+    @staff = Staff.find(params[])
   end
 end
