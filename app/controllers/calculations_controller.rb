6ceaa74d8152c77e61b8d8ded510b76161db6789
宮田昌平 <heyheyshohey0104@gmail.com>
2018-09-12 18:40:16 +0900

    ready-chanpagne-select
    シャンパンの選択のための準備をしました。

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,4 +1,6 @@
 class CalculationsController < ApplicationController
   def index
+    @staffs = Staff.where(user_id: current_user.id)
+    @chanpagne = Chanpagne.where(user_id: current_user.id)
   end
 end
